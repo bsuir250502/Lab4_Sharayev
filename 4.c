15713b090719060a29f7c1d0f58f96b3efeed13d
Eugen <sj2903@mail.ru>
2013-04-18 20:55:36 +0300

    added ring, some defines and field in tree

--- 4.c
+++ 4.c
@@ -4,10 +4,18 @@
 #include "mylib.h"
 
 #define MAX_KEY_LENGTH 8
+#define MAX_STRING_LENGTH 100
+#define MAX_WORD_LENGTH 10
 
+typedef struct ring {
+    char string[MAX_STRING_LENGTH];
+    struct ring *right;
+    struct ring *left;
+} ring_t;
 
 typedef struct node {
     char key[MAX_KEY_LENGTH];
+    ring_t *ring;
     int data;
     struct node *left;
     struct node *right;
@@ -17,6 +25,8 @@ int display_tree(node_t *);
 char read_argument(int, char **);
 int attach_node(node_t * , char *, int );
 int add_node(node_t * );
+int attach_ring(ring_t *);
+int attach_elem(ring_t **, char *);
 
 int main(int argc, char **argv)
 {
@@ -25,14 +35,13 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    node_t root;
-    root.left = NULL;
-    root.right = NULL;
-    root.data = 5;
+    node_t *root;
+    root = (node_t *)calloc(1, sizeof(*root));
+    root->data = 5;
     printf("Specify root key:\n");
-    myfgets(root.key, MAX_KEY_LENGTH);
-    while(add_node(&root));
-    display_tree(&root); 
+    myfgets(root->key, MAX_KEY_LENGTH);
+    while(add_node(root));
+    display_tree(root); 
 
     return 0;
 }
@@ -74,7 +83,7 @@ int add_node(node_t * root)
     data = input_number_in_range(0, 1000);
     attach_node(root, key, data);
 
-    return 0;
+    return 1;
 }
 
 int attach_node(node_t * root, char *key, int data) 
@@ -101,7 +110,47 @@ int attach_node(node_t * root, char *key, int data)
             }
         }
     strncpy(tmp->key, key, MAX_KEY_LENGTH);
+    attach_ring(tmp->ring);
     tmp->data = data;
+
+    return 0;
+}
+
+int attach_ring(ring_t *ring)
+{
+    char buffer[MAX_STRING_LENGTH];
+    ring = (ring_t *) calloc(1, sizeof(*ring));
+    while(1) {
+        printf("Enter string(end, to stop reading):\n");
+        myfgets(buffer, MAX_STRING_LENGTH);
+        if(!strncmp(buffer, "end", 3)){
+            return 0;
+        }
+        attach_elem(&ring, buffer);
+
+    }
+
+    return 0;
+}
+
+int attach_elem(ring_t **ring, char *string) 
+{
+    ring_t *new_elem;
+    new_elem = (ring_t *) calloc(1, sizeof(*new_elem));
+    strncpy(new_elem->string, string, MAX_STRING_LENGTH);
+
+    if(!*ring)
+    {
+        (*ring) = new_elem;
+        (*ring)->right = (*ring);
+        (*ring)->left = (*ring);
+    }
+    else {
+        new_elem = (*ring)->right->left;
+        (*ring)->right->left = new_elem->right;
+        (*ring)->right = new_elem->left;
+        (*ring)->right = new_elem->left;
+    }
 
     return 0;
 }
