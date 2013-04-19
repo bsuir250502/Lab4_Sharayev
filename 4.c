6f3da735c9e43f394429c01ed8758ab3cc49610f
Eugen <sj2903@mail.ru>
2013-04-19 19:30:46 +0300

    added find word funct and fixed some bugs

--- 4.c
+++ 4.c
@@ -15,7 +15,7 @@ typedef struct ring {
 
 typedef struct node {
     char key[MAX_KEY_LENGTH];
-    ring_t *ring;
+    ring_t **ring;
     struct node *left;
     struct node *right;
 } node_t;
@@ -24,9 +24,10 @@ int display_tree(node_t *);
 char read_argument(int, char **);
 int attach_node(node_t * , char *);
 int add_node(node_t * );
-int attach_ring(ring_t *);
+int attach_ring(ring_t **);
 int attach_elem(ring_t **, char *);
 int display_ring(ring_t *);
+int find_word(char *, char *);
 
 int main(int argc, char **argv)
 {
@@ -37,11 +38,12 @@ int main(int argc, char **argv)
 
     node_t *root;
     root = (node_t *)calloc(1, sizeof(*root));
-
+    attach_ring(root->ring);
     printf("Specify root key:\n");
     myfgets(root->key, MAX_KEY_LENGTH);
     while(add_node(root));
-    display_tree(root); 
+    display_tree(root);
+    puts(""); 
 
     return 0;
 }
@@ -62,7 +64,8 @@ int display_tree(node_t * root)
     if (!root) {
         return 0;
     }
-    display_ring(root->ring);
+    printf("(%s)", root->key);
+    //display_ring(*(root->ring));
     display_tree(root->right);
     display_tree(root->left);
 
@@ -87,39 +90,40 @@ int attach_node(node_t * root, char *key)
 {
     node_t *tmp;
     tmp = root;
-
+    while(1) {
         if (0 < strncmp(key, tmp->key, MAX_KEY_LENGTH)) {
             if (tmp->left) {
                 tmp = tmp->left;
             } else {
                 tmp->left= (node_t *)calloc(1,sizeof(*tmp->left));
                 tmp = tmp->left;
+                break;
             }
-        } else {
-            if (tmp->right) {
+        } else if (tmp->right) {
                 tmp = tmp->right;
             } else {
                 tmp->right = (node_t *)calloc(1,sizeof(*tmp->right));
                 tmp = tmp->right;
+                break;
             }
-        }
+    }
     strncpy(tmp->key, key, MAX_KEY_LENGTH);
-    attach_ring(tmp->ring);
+    //attach_ring(tmp->ring);
 
     return 0;
 }
 
-int attach_ring(ring_t *ring)
+int attach_ring(ring_t **ring)
 {
     char buffer[MAX_STRING_LENGTH];
-    ring = (ring_t *) calloc(1, sizeof(*ring));
+    *ring = (ring_t *) calloc(1, sizeof(**ring));
     while(1) {
         printf("Enter string(end, to stop reading):\n");
         myfgets(buffer, MAX_STRING_LENGTH);
         if(!strncmp(buffer, "end", 3)){
             return 0;
         }
-        attach_elem(&ring, buffer);
+        attach_elem(ring, buffer);
 
     }
 
@@ -129,6 +133,7 @@ int attach_ring(ring_t *ring)
 int attach_elem(ring_t **ring, char *string) 
 {
     ring_t *new_elem;
+    printf("work");
     new_elem = (ring_t *) calloc(1, sizeof(*new_elem));
     strncpy(new_elem->string, string, MAX_STRING_LENGTH);
 
@@ -163,4 +168,19 @@ int display_ring(ring_t *ring)
     while(tmp != ring);
 
     return 0;
+}
+
+int find_word(char *str, char *word) 
+{
+    int counter = 0;
+    char *p;
+    p = str;
+    while(p){
+        p = strstr(p, word);
+        if(p) {
+            counter++;
+        }
+    }
+
+    return counter;
 }
